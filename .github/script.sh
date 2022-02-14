455f4d1a4ce2a47b7948527e2f7a1d9fa5e5d23e
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2022-02-14 17:11:21 +0600

    Remove echo MAVEN_USERNAME

--- .github/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ ! -v MAVEN_USERNAME ]]; then
-    echo "MAVEN_USERNAME is not set"
-elif [[ -z "$MAVEN_USERNAME" ]]; then
-    echo "MAVEN_USERNAME is set to the empty string"
-else
-    echo "MAVEN_USERNAME has the value: $MAVEN_USERNAME"
-fi
